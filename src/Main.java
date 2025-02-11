5c6d983debe7380085a01ffa1390d45acadfc164
Logan <mahlerln@mail.uc.edu>
2025-02-11 13:54:29 -0500

    Final Assignment 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+Scanner scanner = new Scanner(System.in);
+boolean playAgain = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+while (playAgain) {
+    String playerAMove = userInput(scanner, "Player A, enter your move (R/P/S): ");
+    String playerBMove = userInput(scanner, "Player B, enter your move (R/P/S): ");
+    String result = determineWinner(playerAMove, playerBMove);
+            System.out.println(result);
+            System.out.print("Play again? (Y/N): ");
+            playAgain = scanner.nextLine().toUpperCase().charAt(0) == 'Y';
+        }
+        scanner.close();
+    }
+
+
+    static String userInput(Scanner scanner, String prompt) {
+        String move;
+        do {
+            System.out.print(prompt);
+            move = scanner.nextLine().toUpperCase();
+        } while (!move.equals("R") && !move.equals("P") && !move.equals("S"));
+        return move;
+    }
+
+
+    static String determineWinner(String playerA, String playerB) {
+        if (playerA.equals(playerB)) {
+            return "It's a tie!";
+        } else if ((playerA.equals("R") && playerB.equals("S")) ||
+                (playerA.equals("P") && playerB.equals("R")) ||
+                (playerA.equals("S") && playerB.equals("P"))) {
+            return "Player A wins!";
+        } else {
+            return "Player B wins!";
         }
     }
 }
